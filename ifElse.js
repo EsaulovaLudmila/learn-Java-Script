295c43c92fbc867f06e4649f3f1439acb9c71349
EsaulovaLudmila <esaulova_l_v@mail.ru>
2023-08-17 22:14:36 +0300

    created ifElse.js

---
+++ ifElse.js
@@ -0,0 +1,13 @@
+let balance = 100;
+const product1Price = 110;
+//------------------------------
+
+document.write("START", "<br>");
+
+if (product1Price <= balance) {
+  document.write("Product purchased");
+} else {
+  document.write("Not enough money");
+}
+
+document.write("<br>", "END");
